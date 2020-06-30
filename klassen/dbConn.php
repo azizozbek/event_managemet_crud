f223bec2a6e0ff38334475faba9350645dec6409
Aziz Ozbek <aziza2772@gmail.com>
2020-06-30 17:40:20 +0200

    rotush

--- klassen/dbConn.php
+++ klassen/dbConn.php
@@ -2,7 +2,7 @@
 define('DBHost', '127.0.0.1');
 define('DBPort', 3306);
 define('DBName', 'eventify');
-define('DBUser', 'root');
-define('DBPassword', '');
+define('DBUser', 'eventify');
+define('DBPassword', 'eventify');
 
 require( __DIR__ . "/PDO.class.php");
